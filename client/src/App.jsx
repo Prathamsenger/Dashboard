0826935f83b716dab765125af7692c5118415880
Pratham singh <singhpratham128@gmail.com>
2025-08-26 18:01:10 +0530

    day 1 setip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-
+import {CssBaseline, ThemeProvider} from "@mui/material"
+import {createTheme } from "@mui/material"
+import { useSelector } from "react-redux"
+import { themeSettings } from "theme"
 
 const App = () => {
+  const mode = useSelector((state)=>state.global.mode )
+  const theme = useMemo(()=> createTheme(themeSettings(mode)),[mode])
   return (
   <>
-  
+  <div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
+  </div>
   
   </>
   )
