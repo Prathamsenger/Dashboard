73fb514492bf5e017e28378c4ca6d44194876d82
Pratham singh <singhpratham128@gmail.com>
2025-08-28 18:07:57 +0530

    day3

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React,{useState} from 'react'
+import { LightModeOutlined, DarkModeOutlined, MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from '@mui/icons-material'
+import FlexBetween from './FlexBetween'
+import { useDispatch } from 'react-redux'
+import { setMode } from '../state'
+import profileImage from "assets/profile.jpeg"
+import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material'
+
+
+
+const Navbar = () => {
+    const dispatch = useDispatch();
+    const theme = useTheme()
+  return <AppBar  
+     sx={{
+        position:"static",
+        background: "none",
+        boxShadow: "none"
+    }} >
+   <Toolbar sx={{justifyContent: "space-betweeen"}}>
+    {/* Left Side*/}
+    <FlexBetween>
+        <IconButton onClick={()=> console.log('open/close sidebar')}>
+            <MenuIcon/>
+            <FlexBetween backgroundColor={theme.palette.background.alt}
+            borderRadius = "9px"
+            gap="3rem"
+            p="0.1rem 1.5rem" >
+           <InputBase placeholder='Search...'/>
+           <IconButton>
+            <Search/>
+           </IconButton>
+            </FlexBetween>
+        </IconButton>
+    </FlexBetween>
+   </Toolbar>
+  </AppBar>
+}
+
+export default Navbar
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import {useState} from "react"
 import{ Box, useMediaQuery } from "@mui/material"
+import {Outlet} from "react-router-dom"
+import { useSelector } from 'react-redux'
+import Navbar from '../layout'
 
 const Layout = () => {
   return (
-    <div>
-      Layout
-    </div>
+   <Box width="100%" height="100%">
+   <Box>
+    <Navbar/>
+    <Outlet/>
+   </Box>
+   </Box>
   )
 }
 
