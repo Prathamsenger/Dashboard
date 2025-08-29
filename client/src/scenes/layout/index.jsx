692417dd706064d3039c15dd7eecd2b2106d5d5d
Pratham singh <singhpratham128@gmail.com>
2025-08-29 17:56:29 +0530

    day 3

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {useState} from "react"
 import{ Box, useMediaQuery } from "@mui/material"
-import {Outlet} from "react-router-dom"
 import { useSelector } from 'react-redux'
-import Navbar from '../layout'
+import Navbar from '../../components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
