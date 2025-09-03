86f82b676fe897b98326b3fe7b4bc396a916dac6
Pratham singh <singhpratham128@gmail.com>
2025-09-03 19:30:08 +0530

    day7

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { configureStore } from '@reduxjs/toolkit'
-import globalReducer from "./state"; // Corrected import path
+import globalReducer from "./state"; 
 import {Provider} from "react-redux"
 
 const store = configureStore({
